395fe602148eb00a13e4ddc283b349fcbe465a6d
Kristaps Dambergs <kristaps.dambergs@gmail.com>
2022-05-19 22:09:39 +0300

    Quick style changes

--- src/components/Calculator/Styled/index.js
+++ src/components/Calculator/Styled/index.js
@@ -1,59 +1,58 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-    display: grid;
-    justify-content: center;
-    align-content: center;
-    width: 232px;
-    margin:40px auto;
-    grid-template-columns: repeat(4, 58px);
-    grid-template-rows: minmax(48px, auto) repeat(5, 48px);
-    box-shadow: 2px 2px 10px #333;
-    border-radius: 10px;
-`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  width: 232px;
+  margin: 40px auto;
+  grid-template-columns: repeat(4, 58px);
+  grid-template-rows: minmax(48px, auto) repeat(5, 48px);
+  box-shadow: 2px 2px 10px #333;
+  border-radius: 10px;
+`;
 
 export const Screen = styled.div`
-    grid-column: 1 / -1;
-    background-color: #454647;
-    display:flex;
-    justify-content: space-around;
-    flex-direction: column;
-    padding: 15px;
-    word-wrap: break-word;
-    word-break: break-all;
-    text-align: right;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-    height:80px;
-`
+  grid-column: 1 / -1;
+  background-color: #454647;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 15px;
+  word-wrap: break-word;
+  word-break: break-all;
+  text-align: right;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  height: 80px;
+`;
 
 export const CurrentNumber = styled.div`
-    color: white;
-    font-size: 3rem;
-`
+  color: white;
+  font-size: 3rem;
+`;
 
 export const Button = styled.button`
-    cursor: pointer;
-    color: white;
-    font-size: 2rem;
-    border: 1px outset #504f54;
-    outline: none;
-    background-color: #737474;
+  cursor: pointer;
+  color: white;
+  font-size: 1.3rem;
+  border: 1px outset #504f54;
+  outline: none;
+  background-color: #737474;
 
-    /* expanded form example 
-    ${ function({gridSpan}){
-        if(gridSpan){
-            return `grid-column: span ${gridSpan}`
-        }
-    }}
+  /* expanded form example 
+    ${function ({ gridSpan }) {
+    if (gridSpan) {
+      return `grid-column: span ${gridSpan}`;
+    }
+  }}
     */
 
-    /* shothand */
-    ${({zero}) => zero && `border-bottom-left-radius: 10px;`} 
+  /* shothand */
+  ${({ zero }) => zero && `border-bottom-left-radius: 10px;`}
 
-    ${({gridSpan}) => gridSpan && `grid-column: span ${gridSpan}`} 
-    ${({operation}) => operation && `background-color: #ff9f09;`} 
-    ${({equals}) => equals && `border-bottom-right-radius: 10px;`} 
-    ${({special}) => special && `background-color: #5e5e5e;`} 
-
-`
+  ${({ gridSpan }) => gridSpan && `grid-column: span ${gridSpan}`} 
+    ${({ operation }) => operation && `background-color: #ff9f09;`} 
+    ${({ equals }) => equals && `border-bottom-right-radius: 10px;`} 
+    ${({ special }) => special && `background-color: #5e5e5e;`}
+`;
--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,122 +1,153 @@
-import React, { useState } from "react"         //react hook useState
-import { Button, Container, CurrentNumber, Screen } from "./Styled"
+import React, { useState } from "react"; //react hook useState
+import { Button, Container, CurrentNumber, Screen } from "./Styled";
 
-export default function Calculator(){
+export default function Calculator() {
+  const [current, setCurrent] = useState(""); //state variables
+  const [previous, setPrevious] = useState("");
+  const [operation, setOperation] = useState("");
 
-    const [current, setCurrent] = useState('');     //state variables
-    const [previous, setPrevious] = useState('');
-    const [operation, setOperation] = useState('');
+  const appendValue = (element) => {
+    const value = element.target.getAttribute("data");
 
-    
-    const appendValue = (element) => {
-        const value = element.target.getAttribute('data')
+    if (value === "," && current.includes(",")) return;
 
-        if(value === ',' && current.includes(',')) return
-        
-        setCurrent(current + value)
+    setCurrent(current + value);
+  };
+
+  const handleClear = () => {
+    setCurrent("");
+    setPrevious("");
+    setOperation("");
+  };
+
+  const percentage = () => {
+    var value;
+
+    if (current !== "") {
+      value = current / 100;
+    } else {
+      return;
     }
 
-    const handleClear = () => {
-        setCurrent('')
-        setPrevious('')
-        setOperation('')
+    setCurrent(value);
+  };
+
+  const positiveNegative = () => {
+    var value;
+
+    if (current !== "" || current > 0) value = -current;
+    setCurrent(value);
+  };
+
+  const chooseOperation = (element) => {
+    if (current === "") return;
+    if (previous !== "") {
+      let value = compute();
+      setPrevious(value);
+    } else {
+      setPrevious(current);
     }
+    setCurrent("");
+    setOperation(element.target.getAttribute("data"));
+  };
 
-    const percentage = () => {
-        var value
-        
-        if(current !== ''){
-            value = current / 100;
-        }
-        else{
-            return
-        }
+  const equals = () => {
+    let value = compute();
+    if (value === undefined || value == null) return;
 
-        setCurrent(value)
+    setCurrent(value);
+    setPrevious("");
+    setOperation("");
+  };
+
+  const compute = () => {
+    let result;
+    let currentNumber = parseFloat(current); //convert string to float
+    let previousNumber = parseFloat(previous);
+
+    if (isNaN(previousNumber) || isNaN(currentNumber)) return;
+
+    switch (operation) {
+      case "÷":
+        result = previousNumber / currentNumber;
+        break;
+      case "x":
+        result = previousNumber * currentNumber;
+        break;
+      case "-":
+        result = previousNumber - currentNumber;
+        break;
+      case "+":
+        result = previousNumber + currentNumber;
+        break;
+      default:
+        return;
     }
+    return result;
+  };
 
-    const positiveNegative = () => {
-        var value
-
-        if(current !== '' || current > 0) value = -current;
-        setCurrent(value)
-    }
-
-    const chooseOperation = (element) => {
-        if(current === '') return
-        if(previous !== ''){
-            let value = compute()
-            setPrevious(value)
-        }
-        else{
-            setPrevious(current)
-        }
-        setCurrent('')
-        setOperation(element.target.getAttribute('data'))
-    }
-
-    const equals = () => {
-        let value = compute();
-        if(value === undefined || value == null) return
-
-        setCurrent(value)
-        setPrevious('')
-        setOperation('')
-    }
-
-    const compute = () => {
-
-        let result
-        let currentNumber = parseFloat(current)     //convert string to float
-        let previousNumber = parseFloat(previous)   
-
-        if(isNaN(previousNumber) || isNaN(currentNumber)) return
-        
-        switch(operation){
-            case '÷':
-                result = previousNumber / currentNumber;
-                break;
-            case 'x':
-                result = previousNumber * currentNumber;
-                break;
-            case '-':
-                result = previousNumber - currentNumber;
-                break;
-            case '+':
-                result = previousNumber + currentNumber;
-                break;
-            default:
-                return
-        }
-        return result;
-    }
-
-
-
-    return (
-        <Container>
-            <Screen>
-                <CurrentNumber>{current}</CurrentNumber>
-            </Screen>
-            <Button onClick={handleClear} special>C</Button>
-            <Button data={'+/-'} onClick={positiveNegative} special>+/-</Button>
-            <Button data={'%'} onClick={percentage} special>%</Button>
-            <Button data={'÷'} onClick={chooseOperation} operation>÷</Button>
-            <Button data={7} onClick={appendValue}>7</Button>
-            <Button data={8} onClick={appendValue}>8</Button>
-            <Button data={9} onClick={appendValue}>9</Button>
-            <Button data={'x'} onClick={chooseOperation} operation>x</Button>
-            <Button data={4} onClick={appendValue}>4</Button>
-            <Button data={5} onClick={appendValue}>5</Button>
-            <Button data={6} onClick={appendValue}>6</Button>
-            <Button data={'-'} onClick={chooseOperation} operation>-</Button>
-            <Button data={1} onClick={appendValue}>1</Button>
-            <Button data={2} onClick={appendValue}>2</Button>
-            <Button data={3} onClick={appendValue}>3</Button>
-            <Button data={'+'} onClick={chooseOperation} operation>+</Button>
-            <Button data={0} gridSpan={2} onClick={appendValue} zero>0</Button>
-            <Button data={','} onClick={appendValue}>,</Button>
-            <Button operation onClick={equals} equals>=</Button>
-        </Container>
-    )
+  return (
+    <Container>
+      <Screen>
+        <CurrentNumber>{current}</CurrentNumber>
+      </Screen>
+      <Button onClick={handleClear} special>
+        c
+      </Button>
+      <Button data={"+/-"} onClick={positiveNegative} special>
+        +/-
+      </Button>
+      <Button data={"%"} onClick={percentage} special>
+        %
+      </Button>
+      <Button data={"÷"} onClick={chooseOperation} operation>
+        ÷
+      </Button>
+      <Button data={7} onClick={appendValue}>
+        7
+      </Button>
+      <Button data={8} onClick={appendValue}>
+        8
+      </Button>
+      <Button data={9} onClick={appendValue}>
+        9
+      </Button>
+      <Button data={"x"} onClick={chooseOperation} operation>
+        x
+      </Button>
+      <Button data={4} onClick={appendValue}>
+        4
+      </Button>
+      <Button data={5} onClick={appendValue}>
+        5
+      </Button>
+      <Button data={6} onClick={appendValue}>
+        6
+      </Button>
+      <Button data={"-"} onClick={chooseOperation} operation>
+        -
+      </Button>
+      <Button data={1} onClick={appendValue}>
+        1
+      </Button>
+      <Button data={2} onClick={appendValue}>
+        2
+      </Button>
+      <Button data={3} onClick={appendValue}>
+        3
+      </Button>
+      <Button data={"+"} onClick={chooseOperation} operation>
+        +
+      </Button>
+      <Button data={0} gridSpan={2} onClick={appendValue} zero>
+        0
+      </Button>
+      <Button data={","} onClick={appendValue}>
+        ,
+      </Button>
+      <Button operation onClick={equals} equals>
+        =
+      </Button>
+    </Container>
+  );
 }
